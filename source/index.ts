230090f21e1bdab13029991471b2ca3bbf63a37f
AsleepRodent <qualitychronology@gmail.com>
2026-06-05 18:01:05 -0600

    refactor: remove local type tickable

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,5 @@
 export { Scheduler } from "./scheduler/scheduler.ts";
 export { Channel } from "./scheduler/channel.ts";
-export type { Tickable } from "./common/types/tickable.ts";
 export { WebTickProvider } from "./providers/web.ts";
 export type { TickProvider } from "./providers/provider.ts";
 export { NodeTickProvider } from "./providers/node.ts";
